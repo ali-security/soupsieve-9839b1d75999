ac30df755cfc171ce128a6aea243fc97fbc143c9
Seal <info@sealsecurity.io>
2026-09-13 08:06:44 +0000

    CVE-2026-49477

--- tests/test_extra/test_attribute.py
+++ tests/test_extra/test_attribute.py
@@ -1,5 +1,10 @@
 """Test attribute selectors."""
+import subprocess
+import sys
 from .. import util
+
+# Maximum time (in seconds) a malformed selector is allowed to take before it is considered hung.
+COMPILE_TIMEOUT = 20
 
 
 class TestAttribute(util.TestCase):
@@ -50,3 +55,50 @@ class TestAttribute(util.TestCase):
             ["div", "0", "1", "2", "3", "pre", "4", "6"],
             flags=util.HTML5
         )
+
+    def assert_fails_fast(self, pattern):
+        """Assert that a bad pattern fails with a syntax error instead of hanging the regular expression engine."""
+
+        # Compile in a separate process. A pattern that triggers catastrophic backtracking cannot be
+        # reliably interrupted with a timer on every platform (`signal.SIGALRM` is not available on Windows).
+        source = '\n'.join(
+            [
+                'import soupsieve as sv',
+                'try:',
+                '    sv.compile({})'.format(pattern),
+                'except sv.SelectorSyntaxError:',
+                '    pass',
+                'else:',
+                "    raise AssertionError('Expected a SelectorSyntaxError')",
+                ''
+            ]
+        )
+
+        try:
+            status = subprocess.run(
+                [sys.executable, '-c', source],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                timeout=COMPILE_TIMEOUT
+            )
+        except subprocess.TimeoutExpired:
+            self.fail('Compiling the pattern did not complete in {} seconds'.format(COMPILE_TIMEOUT))
+        self.assertEqual(status.returncode, 0, status.stdout.decode('utf-8', 'replace'))
+
+    def test_bad_attribute_unclused(self):
+        """Test bad attribute fails for syntax error, not timeout error."""
+
+        # Unclosed, double quoted attribute value: `[a="xxx...`
+        self.assert_fails_fast("'[a=' + chr(34) + ('x' * 300)")
+
+    def test_bad_attribute_unclused_single_quote(self):
+        """Test bad attribute with a single quoted value fails for syntax error, not timeout error."""
+
+        # Unclosed, single quoted attribute value: `[a='xxx...`
+        self.assert_fails_fast("'[a=' + chr(39) + ('x' * 300)")
+
+    def test_bad_attribute_unclused_no_quote(self):
+        """Test bad attribute with an unquoted value fails for syntax error, not timeout error."""
+
+        # Unclosed, unquoted attribute value: `[a=xxx...`
+        self.assert_fails_fast("'[a=' + ('x' * 300)")
